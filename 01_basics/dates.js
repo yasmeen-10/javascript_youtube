26150e2ba9820aadfdb96fcb4a056f7997d06a06
Yasmeen <90993453+yasmeen-10@users.noreply.github.com>
2024-10-13 17:08:41 +0000

    dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,26 @@
+let date=new Date()
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+
+
+// Date : Date is an object in javascript. 
+// JavaScript stores dates as number of milliseconds since January 01, 1970
+const myDate = new Date();
+console.log(myDate) //Output : 2023-10-09T14:38:09.847Z
+// We can use various methods to convert this date into more readable value.
+console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
+console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
+console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+
+console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
+console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
+console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
+
+
+
