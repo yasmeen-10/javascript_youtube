e9a6a936b547e52358ea35cd0d93ce73e79f84ca
Yasmeen <90993453+yasmeen-10@users.noreply.github.com>
2024-10-13 16:16:34 +0000

    nums & strings

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,6 @@
+const score=400
+console.log(score);
+
+const num=new Number(300)
+console.log(num);
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,18 @@
+// let name="egofever"
+// let age=23
+
+// console.log(name + "  " + age);
+
+// console.log(`${name} ${age}`);
+
+// console.log(name.charAt(5));
+// console.log(name.indexOf('y'));
+
+var str1="yasmeen"
+console.log(str1.slice(0,4));
+console.log(str1.slice(2));
+console.log(str1.slice(-3));
+
+console.log(str1.substring(0,4));
+console.log(str1.substring(2));
+console.log(str1.substring(-3));
