8a71d8e0813c1388b770611a9e5e1ebf54480951
Yasmeen <90993453+yasmeen-10@users.noreply.github.com>
2024-10-14 07:45:46 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+//ARRAY
+const myArr=[1,2,3,4,5]
+//console.log(myArr);
+
+// const arr1=new Array(21,22,212)
+// console.log(arr1);
+
+//ARRAY METHODS
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(22)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+//console.log(myArr.includes(1));
+
+// const newArr=myArr.join()
+// console.log(myArr);
+// console.log(newArr); //string format
+
+// console.log(typeof(newArr));
+// console.log(newArr.charAt(0));
+
+//SLICE & SPLICE
+
+//SLICE
+// const a=[1,2,3,4,5]
+// console.log("original array before slicing",a);
+
+// const sa=a.slice(0,3)
+// console.log("sliced array",sa);
+// console.log("original array after slicing",a);
+
+//SPLICE
+
+const oa=[11,12,13,14,15]
+console.log("original array before splicing",oa);
+
+const sa=oa.splice(1,3)
+console.log("spliced array",sa);
+console.log("original array after splicing",oa);
+
+
+
+
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+const fruits=["apple","orange","mango","kiwi"]
+const cars=["BMW","Audi","Nano","Thar"]
+
+//fruits.push(cars)
+//console.log(fruits);
+
+// const new_array=fruits.concat(cars)
+// console.log(new_array);
+
+// const new_Arr=[...fruits,...cars]
+// console.log(new_Arr);
+
+// const a=[1,2,3,4,[5,6,7],8,[11,12,[111,121,122]]]
+// const b=a.flat(Infinity)
+// console.log(b);
+
+// console.log(Array.isArray("Yashu"))
+// console.log(Array.from("Yasmeen"));
+// console.log(Array.from({name:"Yashu"}));
+
+// let s1=100
+// let s2=200
+// let s3=300
+// console.log(Array.of(s1,s2,s3));
+
+console.log(Array.of(1,2,3,4,5));
+console.log(Array.from('123'));
+
+
+
+
+
+
