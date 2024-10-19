c857686464af8c99701273a03d5614e0a368aa99
Yasmeen <90993453+yasmeen-10@users.noreply.github.com>
2024-10-19 08:43:04 +0000

    control_flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=5
+switch(month){
+    case 1:console.log("January");
+           break;
+    case 2:console.log("February");
+           break;
+    case 3:console.log("March");
+           break;
+    case 4:console.log("April");
+           break;
+    case 5:console.log("May");
+           break;
+    default:console.log("default");
+           break;
+    
+}
